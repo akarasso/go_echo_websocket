73cf0bbc1fb43f709b95b8fc42f43a74a1e46739
Alexandre KARASSOULOFF <akarasso@e1r11p5.42.fr>
2019-01-05 18:39:50 +0100

    GetConn

--- getconn.go
+++ getconn.go
@@ -9,7 +9,6 @@ import (
 func (s *Websocket_s) GetConn(by interface{}) (*s_client, error) {
 	switch by.(type) {
     case *websocket.Conn:
-        log.Println("Try to get conn by ws")
         if conn, exist := s.clients.byConn[by.(*websocket.Conn)]; exist {
 			return conn, nil
 		}
@@ -19,24 +18,21 @@ func (s *Websocket_s) GetConn(by interface{}) (*s_client, error) {
 			return conn, nil
 		}
 	case int64:
-        log.Println("Try to get conn by int64")
         ids := strconv.FormatInt(by.(int64), 16)
 		if conn, exist := s.clients.byID[ids]; exist {
 			return conn, nil
 		}
 	case int:
-        log.Println("Try to get conn by int")
         ids := strconv.Itoa(by.(int))
 		if conn, exist := s.clients.byID[ids]; exist {
 			return conn, nil
 		}
 	case string:
-        log.Println("Try to get conn by string")
 		if conn, exist := s.clients.byID[by.(string)]; exist {
 			return conn, nil
 		}
     default:
-        log.Println("Try to get conn with non accepted type")
+        log.Printf("Try to get conn with non accepted type:%T\n", by)
 	}
     return nil, nil
 }
